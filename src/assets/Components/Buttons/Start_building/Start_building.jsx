9d4b0dfbcdcc35ce84c2b0341a5f9750c997017a
Abhijith834 <abhijithsaji834@gmail.com>
2024-07-04 17:02:44 +0100

    Section 1 initiated

---
+++ src/assets/Components/Buttons/Start_building/Start_building.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Start_building.css";
+
+const Start_building = () => {
+  return <div className="start_building">start building</div>;
+};
+
+export default Start_building;
--- src/assets/Components/Section1/Section1.jsx
+++ src/assets/Components/Section1/Section1.jsx
@@ -1,28 +1,60 @@
 import React from "react";
 import "./Section1.css";
-import { Button2 } from "../Buttons/Button2/Button2";
+import leftArrow from "../../svg/Left arrow.svg";
+import rightArrow from "../../svg/Right arrow.svg";
+import hero_image from "../../images/homepage.webp";
+import Start_building from "../Buttons/Start_building/Start_building";
 
 function Section1() {
   return (
     <div className="section1">
       <div className="intro">
-        <h1>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam,
-          maiores.
-        </h1>
-        <h3>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          Necessitatibus tempora laborum unde ad quaerat amet, quisquam eum
-          nulla. Fugit vel excepturi nihil saepe. Debitis, eveniet. Cum delectus
-          veniam obcaecati numquam.
-        </h3>
-        <Button2 />
+        <div className="intro_content">
+          <div className="intro_text">
+            <h1>
+              Build the <span className="highlight">perfect one</span> that
+              suits you
+              <br />
+              It’s only a <span className="highlight">few clicks</span> away
+            </h1>
+
+            <p className="hero_text_1">
+              The CRM is designed with the end-user in mind, ensuring that it
+              fits various business models and user requirements comfortably.
+            </p>
+            <p className="hero_text_2">
+              A hassle-free implementation process, which is a significant
+              advantage for businesses looking to get started with minimal
+              downtime.
+            </p>
+            <div className="hero_button">
+              <Start_building />
+            </div>
+          </div>
+          <div className="arrows">
+            <div className="arrow_left">
+              <img
+                src={leftArrow}
+                alt="Left arrow"
+                className="overlay-svg svg1"
+              />
+            </div>
+            <div className="arrow_right">
+              <img
+                src={rightArrow}
+                alt="Right arrow"
+                className="overlay-svg svg2"
+              />
+            </div>
+          </div>
+        </div>
       </div>
       <div className="image">
-        <h1>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus,
-          eligendi.
-        </h1>
+        <img
+          src={hero_image}
+          alt="Descriptive Alt Text"
+          className="hero_image"
+        />
       </div>
     </div>
   );
